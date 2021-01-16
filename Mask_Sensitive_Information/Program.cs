616c690d3310acc966f10c780242d5dfc1ca3e2b
Sam Mudick <smudick@gmail.com>
2021-01-16 13:01:53 -0600

    updated

--- Mask_Sensitive_Information/Program.cs
+++ Mask_Sensitive_Information/Program.cs
@@ -6,13 +6,18 @@ namespace Mask_Sensitive_Information
     {
         static void Main(string[] args)
         {
-            // Mask Sensitive Information
-
             Console.WriteLine("Enter a secret code that's at least five characters long");
             var secretCode = Console.ReadLine();
+            if (secretCode.Length >= 5)
+            {
             var breakPoint = secretCode.Length - 4;
-            var maskedCode = new String('*', breakPoint) + secretCode.Substring(breakPoint);
+            var maskedCode = new string('*', breakPoint) + secretCode.Substring(breakPoint);
             Console.WriteLine(maskedCode);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Code: Code Must Be At Least 5 Characters Long");
+            }
 
         }
     }
