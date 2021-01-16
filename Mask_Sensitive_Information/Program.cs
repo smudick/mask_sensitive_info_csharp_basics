1c5478b20e7fa3478e653b2c7353f5619dacda11
Sam Mudick <smudick@gmail.com>
2021-01-15 22:14:15 -0600

    mask sensitive info

---
+++ Mask_Sensitive_Information/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Mask_Sensitive_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mask Sensitive Information
+
+            Console.WriteLine("Enter a secret code that's at least five characters long");
+            var secretCode = Console.ReadLine();
+            var breakPoint = secretCode.Length - 4;
+            var maskedCode = new String('*', breakPoint) + secretCode.Substring(breakPoint);
+            Console.WriteLine(maskedCode);
+
+        }
+    }
+}
